077a769cc20e6ffb33d1901a8af3957dfd60ca9c
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-08-19 20:52:19 +0300

    commit

--- app.js
+++ app.js
@@ -80,11 +80,12 @@ document.getElementById('leftButton').addEventListener('click', () => {
 
 document.getElementById('rightButton').addEventListener('click', () => {
     charX += 10;
-    if (charX < 0) {
-        charX = 0;
+    if (charX < 100) {
+        charX = 100;
     }
     updateCharPosition();
 });
+
 
 
 setInterval(datesFalling, 2000);
